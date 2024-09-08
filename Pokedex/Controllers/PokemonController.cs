f227bff247a3a67961c147835b7a3dbe487b352c
Tammam <mohamed.tammam18@gmail.com>
2024-09-08 20:03:58 +0200

    Added Controllers

---
+++ Pokedex/Controllers/PokemonController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pokedex.Services;
+
+namespace Pokedex.Controllers
+{
+
+        [ApiController]
+        [Route("api/pokemon")]
+        public class PokemonController : ControllerBase
+        {
+            private readonly IPokemonService _pokemonService;
+
+            public PokemonController(IPokemonService pokemonService)
+            {
+                _pokemonService = pokemonService;
+            }
+
+            [HttpGet("{name}")]
+            public async Task<IActionResult> GetPokemon(string name)
+            {
+                var pokemon = await _pokemonService.GetPokemonInfoAsync(name);
+                if (pokemon == null)
+                {
+                    return NotFound(new { message = "Pokémon not found" });
+                }
+                return Ok(pokemon);
+            }
+
+            [HttpGet("translated/{name}")]
+            public async Task<IActionResult> GetTranslatedPokemon(string name)
+            {
+                var pokemon = await _pokemonService.GetPokemonInfoAsync(name);
+                if (pokemon == null)
+                {
+                    return NotFound(new { message = "Pokémon not found" });
+                }
+
+                var translatedDescription = await _pokemonService.TranslatePokemonDescriptionAsync(pokemon);
+                pokemon.Description = translatedDescription;
+                return Ok(pokemon);
+            }
+        }
+    
+}
