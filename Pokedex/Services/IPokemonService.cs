16d7dfadd8f79e38d2b4ab7a49d704c146fa6336
Tammam <mohamed.tammam18@gmail.com>
2024-09-08 20:02:58 +0200

    Added Services

---
+++ Pokedex/Services/IPokemonService.cs
@@ -0,0 +1,10 @@
+﻿using Pokedex.Models;
+
+namespace Pokedex.Services
+{
+    public interface IPokemonService
+    {
+        Task<Pokemon> GetPokemonInfoAsync(string pokemonName);
+        Task<string> TranslatePokemonDescriptionAsync(Pokemon pokemon);
+    }
+}
---
+++ Pokedex/Services/PokemonService.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using Pokedex.DAOs;
+using Pokedex.Models;
+
+namespace Pokedex.Services
+{
+    public class PokemonService : IPokemonService
+    {
+        private readonly IPokemonDAO _pokemonDAO;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public PokemonService(IPokemonDAO pokemonDAO, IHttpClientFactory httpClientFactory)
+        {
+            _pokemonDAO = pokemonDAO;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<Pokemon> GetPokemonInfoAsync(string pokemonName)
+        {
+            return await _pokemonDAO.GetPokemonInfoAsync(pokemonName);
+        }
+
+        public async Task<string> TranslatePokemonDescriptionAsync(Pokemon pokemon)
+        {
+            if (string.IsNullOrEmpty(pokemon.Description) || pokemon.Description == "Description not available")
+            {
+                return "Description not available for translation.";
+            }
+
+            var translationType = pokemon.IsLegendary || pokemon.Habitat.Name.ToLower() == "cave" ? "yoda" : "shakespeare";
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync($"https://api.funtranslations.com/translate/{translationType}.json?text={pokemon.Description}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var translation = JsonConvert.DeserializeObject<Translation>(content);
+                return translation!.Contents.Translated;
+            }
+
+            return pokemon.Description;
+        }
+    }
+}
