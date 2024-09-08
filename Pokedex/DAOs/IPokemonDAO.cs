f14815979cca48e3070ff1753687f30ee9843624
Tammam <mohamed.tammam18@gmail.com>
2024-09-08 19:55:35 +0200

    Added DAOs

---
+++ Pokedex/DAOs/IPokemonDAO.cs
@@ -0,0 +1,9 @@
+﻿using Pokedex.Models;
+
+namespace Pokedex.DAOs
+{
+    public interface IPokemonDAO
+    {
+        Task<Pokemon> GetPokemonInfoAsync(string pokemonName);
+    }
+}
---
+++ Pokedex/DAOs/PokemonDAO.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json.Linq;
+using Pokedex.Models;
+
+namespace Pokedex.DAOs
+{
+    public class PokemonDAO : IPokemonDAO
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public PokemonDAO(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<Pokemon> GetPokemonInfoAsync(string pokemonName)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync($"https://pokeapi.co/api/v2/pokemon-species/{pokemonName}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var pokemonData = JObject.Parse(json);
+
+                var pokemon = new Pokemon
+                {
+                    Name = pokemonData["name"].ToString(),
+                    IsLegendary = pokemonData["is_legendary"].ToObject<bool>(),
+                    Habitat = pokemonData["habitat"] != null
+                        ? new Habitat { Name = pokemonData["habitat"]["name"].ToString() }
+                        : new Habitat { Name = "unknown" }
+                };
+
+                var flavorTextEntries = pokemonData["flavor_text_entries"];
+                if (flavorTextEntries != null)
+                {
+                    foreach (var entry in flavorTextEntries)
+                    {
+                        if (entry["language"]["name"].ToString() == "en")
+                        {
+                            var flavorText = entry["flavor_text"].ToString();
+
+                            if (!string.IsNullOrEmpty(flavorText))
+                            {
+                                pokemon.Description = flavorText.Replace("\n", " ").Replace("\f", " ");
+                            }
+                            break;
+                        }
+                    }
+                }
+
+                if (string.IsNullOrEmpty(pokemon.Description))
+                {
+                    pokemon.Description = "Description not available";
+                }
+
+                return pokemon;
+            }
+
+            return null;
+        }
+    }
+
+}
